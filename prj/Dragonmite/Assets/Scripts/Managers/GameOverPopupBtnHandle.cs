28d21f6bfbe7a5a729d87bde00605c9219517484
chicken-developer <quynh.fullstackdev@gmail.com>
2021-03-28 02:15:00 +0700

    [Quynh] Add scene flow

---
+++ prj/Dragonmite/Assets/Scripts/Managers/GameOverPopupBtnHandle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverPopupBtnHandle : MonoBehaviour
+{
+    private Button replayGameBtn;
+    private Button quitMatch;
+    void ReplayGameOnClicked()
+    {
+        Debug.Log("Enter multi mode");
+       //TODO: Replay btn handle
+    }
+   
+
+    void QuitMatchOnClicked()
+    {
+        Debug.Log("Quit game");
+        //TODO: Quit match handle
+    }
+    void Awake()
+    {
+        replayGameBtn = GameObject.FindGameObjectWithTag("replay").GetComponent<Button>();
+        quitMatch = GameObject.FindGameObjectWithTag("quitmatch").GetComponent<Button>();
+    }
+    void Start()
+    {
+        replayGameBtn.onClick.AddListener(ReplayGameOnClicked);
+        quitMatch.onClick.AddListener(QuitMatchOnClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ prj/Dragonmite/Assets/Scripts/Managers/MainMenuUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Button singleModeBtn;
+    private Button multiModeBtn;
+    private Button quitBtn;
+
+    void SingleBtnOnClicked()
+    {
+        Debug.Log("Enter single mode");
+        SceneManager.LoadScene("DragonMite");
+    }
+
+    void MultiBtnOnClicked()
+    {
+        Debug.Log("Enter multi mode");
+        SceneManager.LoadScene("MultiMode");
+    }
+
+    void QuitBtnOnClicked()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+    void Awake()
+    {
+        singleModeBtn = GameObject.FindGameObjectWithTag("singleModeBtn").GetComponent<Button>();
+        multiModeBtn = GameObject.FindGameObjectWithTag("multiModeBtn").GetComponent<Button>();
+        quitBtn = GameObject.FindGameObjectWithTag("quitBtn").GetComponent<Button>();
+    }
+    void Start()
+    {
+        singleModeBtn.onClick.AddListener(SingleBtnOnClicked);
+        multiModeBtn.onClick.AddListener(MultiBtnOnClicked);
+        quitBtn.onClick.AddListener(QuitBtnOnClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
